d97dafaedf504c38ce87e54c4612beb4ca073fc1
Alexander Potemkin <potemkin_av7@yandex.ru>
2024-04-30 01:13:43 +0300

    added createPersonFromCSV method to the CSVPersonDataReader class

--- src/main/java/org/example/CSVPersonDataReader.java
+++ src/main/java/org/example/CSVPersonDataReader.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class CSVPersonDataReader {
     private String csvFilePath;
     private char separator;
@@ -7,5 +11,21 @@ public class CSVPersonDataReader {
     public CSVPersonDataReader(String csvFilePath, char separator) {
         this.csvFilePath = csvFilePath;
         this.separator = separator;
+    }
+
+    private Person createPersonFromCSV(String[] data) {
+        try {
+            int id = Integer.parseInt(data[0]);
+            String name = data[1];
+            char sex = data[2].charAt(0);
+            LocalDate birthDate = LocalDate.parse(data[3], DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+            String department = data[4];
+            int salary = Integer.parseInt(data[5]);
+
+            return new Person(id, name, sex, department, salary, birthDate);
+        } catch (Exception e) {
+            System.err.println("Error parsing CSV data: " + e.getMessage());
+            return null;
+        }
     }
 }
