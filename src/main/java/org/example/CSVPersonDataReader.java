fa267033522734a2b4c0b1488c5330dbae43fb4e
Alexander Potemkin <potemkin_av7@yandex.ru>
2024-04-30 01:54:58 +0300

    main function are implemented

--- src/main/java/org/example/CSVPersonDataReader.java
+++ src/main/java/org/example/CSVPersonDataReader.java
@@ -9,7 +9,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
