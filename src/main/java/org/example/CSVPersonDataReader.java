5f8c1a405ffad407957af49f7bcda5e164580d03
Alexander Potemkin <potemkin_av7@yandex.ru>
2024-04-30 01:45:38 +0300

    added readCSVFile method to the CSVPersonDataReader class

--- src/main/java/org/example/CSVPersonDataReader.java
+++ src/main/java/org/example/CSVPersonDataReader.java
@@ -1,16 +1,52 @@
 package org.example;
 
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CSVPersonDataReader {
-    private String csvFilePath;
-    private char separator;
+    private final String csvFilePath;
+    private final char separator;
+    private List<Person> personList;
 
     public CSVPersonDataReader(String csvFilePath, char separator) {
         this.csvFilePath = csvFilePath;
         this.separator = separator;
+        this.personList = new ArrayList<>();
+    }
+
+    public void readCSVFile() throws IOException {
+
+        try (InputStream in = getClass().getClassLoader().getResourceAsStream(csvFilePath);
+             CSVReader reader = in == null ? null :
+                     new CSVReaderBuilder(new InputStreamReader(in)).
+                             withCSVParser(new CSVParserBuilder().
+                                     withSeparator(separator).build()).build()) {
+            if (reader == null) {
+                throw new FileNotFoundException(csvFilePath);
+            }
+            String[] headers = reader.readNext();
+            String[] nextLine;
+            while ((nextLine = reader.readNext()) != null) {
+                Person person = createPersonFromCSV(nextLine);
+                if (person != null) {
+                    personList.add(person);
+                }
+            }
+        } catch (CsvValidationException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private Person createPersonFromCSV(String[] data) {
@@ -27,5 +63,9 @@ public class CSVPersonDataReader {
             System.err.println("Error parsing CSV data: " + e.getMessage());
             return null;
         }
+    }
+
+    public List<Person> getPersonList() {
+        return personList;
     }
 }
