e9529b6fbe011b9f1106549af0c0b4873d4e00c0
Alexander Potemkin <potemkin_av7@yandex.ru>
2024-04-29 20:01:29 +0300

    Person class fields are implemented

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,4 +1,12 @@
 package org.example;
 
+import java.time.LocalDate;
+
 public class Person {
+    private int id;
+    private String name;
+    private char gender;
+    private String department;
+    private int salary;
+    private LocalDate birthDate;
 }
