c2289d9fd31107ee1a326630bdb5ceafb3a18c73
Alexander Potemkin <potemkin_av7@yandex.ru>
2024-04-30 01:54:04 +0300

    main function are implemented

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,31 @@
 package org.example;
 
+import java.io.IOException;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String csvFilePath = "foreign_names.csv";
+        char separator = ';';
+
+        CSVPersonDataReader csvPersonDataReader =
+                new CSVPersonDataReader(csvFilePath, separator);
+
+        try {
+            csvPersonDataReader.readCSVFile();
+            List<Person> personList = csvPersonDataReader.getPersonList();
+            for (Person person : personList) {
+                System.out.println(
+                        person.getId() + ", " +
+                        person.getName() + ", " +
+                        person.getSex() + ", " +
+                        person.getBirthDate() + ", " +
+                        person.getDepartment() + ", " +
+                        person.getSalary()
+                );
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading CSV file: " + e.getMessage());
+        }
     }
 }
