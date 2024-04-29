263d56c165beb75e408377b1faf2bfd3af63b140
Alexander Potemkin <potemkin_av7@yandex.ru>
2024-04-30 00:56:47 +0300

    CSVPersonDataReader class fields and constructor are implemented

--- src/main/java/org/example/CSVPersonDataReader.java
+++ src/main/java/org/example/CSVPersonDataReader.java
@@ -1,4 +1,11 @@
 package org.example;
 
 public class CSVPersonDataReader {
+    private String csvFilePath;
+    private char separator;
+
+    public CSVPersonDataReader(String csvFilePath, char separator) {
+        this.csvFilePath = csvFilePath;
+        this.separator = separator;
+    }
 }
