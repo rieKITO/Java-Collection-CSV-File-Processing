7e747d158caa9b74c6d0e3d19caabb8b45d95402
Alexander Potemkin <potemkin_av7@yandex.ru>
2024-04-30 01:14:33 +0300

    Person class constructor are implemented

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -5,8 +5,24 @@ import java.time.LocalDate;
 public class Person {
     private int id;
     private String name;
-    private char gender;
+    private char sex;
     private String department;
     private int salary;
     private LocalDate birthDate;
+
+    public Person(
+            int id,
+            String name,
+            char sex,
+            String department,
+            int salary,
+            LocalDate birthDate
+    ) {
+        this.id = id;
+        this.name = name;
+        this.sex = sex;
+        this.department = department;
+        this.salary = salary;
+        this.birthDate = birthDate;
+    }
 }
