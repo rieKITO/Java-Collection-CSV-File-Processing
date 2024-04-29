ee1a8bbfbecd156661647ef07b4f2d82a85115ec
Alexander Potemkin <potemkin_av7@yandex.ru>
2024-04-30 01:14:16 +0300

    Department class constructor are implemented

--- src/main/java/org/example/Department.java
+++ src/main/java/org/example/Department.java
@@ -3,4 +3,12 @@ package org.example;
 public class Department {
     private int id;
     private String name;
+
+    public Department(
+            int id,
+            String name
+    ) {
+        this.id = id;
+        this.name = name;
+    }
 }
